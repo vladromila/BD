43e7344456130c546cfcce886d3816ebc10e54c1
Romila VLad <romilavlad2001@gmail.com>
2021-12-26 00:54:03 +0200

    reboot after server update+ input validation on register

--- SERVER/src/globalHandler.cpp
+++ SERVER/src/globalHandler.cpp
@@ -132,26 +132,17 @@ std::string registerUser(std::string email, std::string password, std::string pa
     int initial_query_stat;
     // sprintf(initial_query, "SELECT firstName FROM users WHERE firstName='%s'", email.c_str());
 
-    initial_query_stat = mysql_query(con, "SELECT firstName FROM users WHERE firstName='romilavlad2001@gmail.com';");
-    if (initial_query_stat != 0)
+    mysql_query(con, "SELECT firstName FROM users WHERE firstName='romilavlad2001@gmail.com';");
+    dbRes = mysql_store_result(con);
+    if (mysql_num_rows(dbRes))
     {
-        dbRes = mysql_store_result(con);
-        if (mysql_num_rows(dbRes))
-        {
 
-            res["email"] = "A user with this email address already exists.";
-            return res.dump();
-        }
-        else
-            return res.dump();
+        res["email"] = "A user with this email address already exists.";
+        return res.dump();
     }
     else
-    {
-        res["passwordConfirm"] = "Error making the request. Please try again.";
         return res.dump();
-        
-    }
-    return "adkmafsdnofs";
+
     if (firstName.size() < 4)
     {
         res["firstNameError"] = "The first name is too short.";
